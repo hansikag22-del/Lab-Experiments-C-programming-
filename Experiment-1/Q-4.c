ffd4e53eef529197e8f4d680d259ec5731155233
Hansika Gupta <hansikag22@gmail.com>
2025-12-14 20:16:29 +0530

    Add files via upload

---
+++ Experiment-1/Q-4.c
@@ -0,0 +1,19 @@
+//4. Write a C program to add two numbers, take number from user.
+
+#include <stdio.h>
+int main()
+{
+    int a,b,sum;
+    
+    printf("//Hansika Gupta\n");
+    printf("Enter first no.");
+    scanf("%d",&a);
+    
+    printf("Enter second no.");
+    scanf("%d",&b);
+    
+    sum=a+b;
+    
+    printf("Addition: %d\n",sum);
+    return 0;
+}
