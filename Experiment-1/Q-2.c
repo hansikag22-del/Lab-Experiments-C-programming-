c2ac9358b7856d8c878297ab7c4eac06ed7f6f9d
Hansika Gupta <hansikag22@gmail.com>
2025-12-14 20:15:32 +0530

    Add files via upload

---
+++ Experiment-1/Q-2.c
@@ -0,0 +1,13 @@
+//2. Write a C Program to print the address in multiple lines (new line).
+
+#include <stdio.h>
+int main()
+{
+    printf("Hansika Gupta\n");
+    printf("F block girls hostel\n");
+    printf("Bhidoli,Near dudha devi mandir\n");
+    printf("Dehradun\n");
+    printf("248007\n");
+
+    return 0;
+}
