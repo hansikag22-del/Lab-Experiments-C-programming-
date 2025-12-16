07b9038e6ea651224bb64ca223ef1721ed567a81
Hansika Gupta <hansikag22@gmail.com>
2025-12-16 22:39:08 +0530

    Add files via upload

---
+++ Experiment-15/Q1.c
@@ -0,0 +1,62 @@
+// arith.c
+#include "arith.h"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int divide(int a, int b) {
+    if (b == 0)
+        return 0;  // simple error handling
+    return a / b;
+}
+
+
+// arith.h
+#ifndef ARITH_H
+#define ARITH_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int divide(int a, int b);
+
+
+
+
+// main.c
+#include <stdio.h>
+#include "arith.h"
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Add: %d\n", add(a, b));
+    printf("Sub: %d\n", sub(a, b));
+    printf("Mul: %d\n", mul(a, b));
+    printf("Div: %d\n", divide(a, b));
+
+    return 0;
+}
+
+#endif
+
+
+
+
+
+
+
+
+
